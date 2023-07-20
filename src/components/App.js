47fd378deb3dad455020e7349bdf6a85da7ac023
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-20 08:15:32 +0300

    contacts in app js change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+// import ContactCard from "./ContactCard";
+import ContactList from "./ContactList";
+import AddContact from "./AddContact";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "prince",
+      email: "Hope",
+    },
+    {
+      id: "1",
+      name: "prince",
+      email: "Hope",
+    },
+    {
+      id: "1",
+      name: "prince",
+      email: "Hope",
+    },
+  ];
+
   return (
-   <div>
-    Hello world
-   </div>
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
   );
 }
 
---
+++ src/components/ContactList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactList = () =>{
+    return (
+        <div ui celled list>
+                Contact List
+        </div>
+    )
+}
+
+export default ContactList;
